e3bea718c05b0607e3dea2bb8a2c3382aae4fc16
Jim Strother <jim.strother@gmail.com>
2019-08-24 15:12:03 -0700

    getting things set up for skill set section

---
+++ src/data/skillsData.js
@@ -0,0 +1,47 @@
+functional programming
+  1 year
+
+git hub
+  3 years
+
+javascript
+  3 years
+
+mongoDB
+  2 years
+
+mongoose
+  2 years
+
+NodeJS
+  3 years
+
+express
+  3 years
+
+web development
+  3 years
+
+react
+  2 years
+
+redux
+  2 years
+
+html
+  4 years
+
+CSS
+  4 years
+
+Sass/SCSS
+  3 years
+
+Vue/Vuex
+  1 year
+
+typescript
+  1 year
+
+java
+  less than 1 year
